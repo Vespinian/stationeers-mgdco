8613d8828f7ca510955c9fd46935e729844f0983
Vespinian <vespinian@proton.me>
2024-08-30 21:33:54 -0400

    Limited scope of settable devices to logic mem and logic readers

--- MGDCOPatches.cs
+++ MGDCOPatches.cs
@@ -75,7 +75,7 @@ namespace MoreGasDisplayConsoleOptions
 				GasDisplayExtenderDict.gasDisplayDict[__instance].SetableDevices.Clear();
 				foreach (Device device in __instance.LinkedDevices)
 				{
-					if (__instance.IsDeviceConnected(device))
+					if (((device is LogicMemory) || (device is LogicReader)) && (__instance.IsDeviceConnected(device)))
 					{
 						ISetable setableDevice = device as ISetable;
 						if (setableDevice != null)
